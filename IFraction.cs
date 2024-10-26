b6ba5a8a19be23630c24f85a8115f11c660d0525
Andrewsis <151087281+Andrewsis@users.noreply.github.com>
2024-10-26 10:28:01 +0200

    Update IFraction.cs

--- IFraction.cs
+++ IFraction.cs
@@ -1,1 +1,17 @@
+using System;
 
+namespace FractionWork
+{
+    public interface IFraction
+    {
+        int numerator { get; set; }
+        int denominator { get; set; }
+
+        Fraction Add(Fraction other);
+        Fraction Subtract(Fraction other);
+        Fraction Multiply(Fraction other);
+        bool Equals(Fraction other);
+        Fraction MultiplyByInteger(int integer);
+        Fraction SubtractInteger(int integer);
+    }
+}
